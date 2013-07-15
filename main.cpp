2731ce93a742d802358c3800027458bdbdc252fc
Dariusz Bruj <dariusz.bruj@gmail.com>
2013-07-15 12:52:08 +0200

    Changed thread priority to max. Added some comments.

--- main.cpp
+++ main.cpp
@@ -16,6 +16,15 @@
 int main(int argc, char **argv)
 {
 
+	// Set max priority.
+    int policy;
+    struct sched_param param;
+
+    pthread_getschedparam(pthread_self(), &policy, &param);
+    param.sched_priority = sched_get_priority_max(policy);
+    pthread_setschedparam(pthread_self(), policy, &param);
+
+    // Displaying welcome text.
 	std::cout << "A10Lib ver. 0.1" << std::endl;
 
 	// Create object.
@@ -28,10 +37,12 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	std::cout << " Select PD0 as an output." << std::endl;
 	// Set pin as an output.
 	a10.gpio.select(A10::GPIO::PortPin::PD0, A10::GPIO::PinSelect::Output);
 
 	// Fast toggle
+	std::cout << " Toggle PD0 pin." << std::endl;
 	volatile uint32_t* 	DAT = a10.gpio.datareg(A10::GPIO::PortPin::PD0);
 	uint32_t 			PIN = 0x1 << a10.gpio.pinnum(A10::GPIO::PortPin::PD0);
 
